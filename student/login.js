bef03fb3532f90390036bdbf504f5f5519d4942c
Anika <32257753+anika107@users.noreply.github.com>
2020-03-19 12:11:15 +0600

    Add files via upload

---
+++ student/login.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const db = require('../database');
+const bp = require('body-parser');
+const hash = require('password-hash');
+const { check, validationResult } = require('express-validator');
+
+const app = express();
+app.use(bp.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+
+app.set('view engine','ejs');
+app.use(bp.json());
+
+//if(req.session.user == "student"){
+    app.post('/login',[
+        check('username', 'The username field is empty.').not().isEmpty().custom((value) =>{
+            return db.execute('select username from student where username = ?', [value]).then(([rows]) =>{
+            if(rows.length == 1){
+               return true;
+            }
+           
+            
+            return Promise.reject('The username does not exists.')
+        })
+    })
+    
+
+], (req, res)=>{
+        console.log(req.body);
+         const result = validationResult(req);
+         const username = req.body.username;
+         const password = req.body.password;
+         console.log(username)
+         console.log(password);
+
+         //const {username, password} = req.body;
+        // if(result.isEmpty()){
+             db.execute('select password from student where username = ?',[username]).then(([rows]) =>{
+                 var hash_pass = hash.generate(password);
+                 console.log(hash_pass);
+                 console.log(rows[0].password);
+                 if(hash.verify(password, rows[0].password)){
+                    // req.session.username = username;
+                     res.render('student-dashboard');
+
+                 }
+                 else{
+                     res.render('login-register', {
+                         login_error: 'Invalid password'
+                     })
+                     
+                 }
+             }) 
+         }
+         /*else{
+            var err = result.errors;
+            console.log(err);
+           // reg_log = false;
+            
+           /* for (var key in err) {
+                console.log(err[key].msg);
+            }*/
+          //  console.log(allErrors)
+           /*  res.render('login-register',{
+                 login_error:err,
+                 old_data:req.body,
+               
+     
+             });
+         }
+       
+   
+        //  res.render('student-dashboard')
+    })
+
+//}
+*/
+    )
+module.exports = app;
