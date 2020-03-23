284f8173856627f4fff6bb24fd63c7c30b4a27c6
Anika <32257753+anika107@users.noreply.github.com>
2020-03-23 17:34:30 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -4,48 +4,23 @@ const session = require('./session');
 const db = require('./database')
 const student_register = require('./student/register');
 const student_login = require('./student/login');
+const student_logout = require('./student/logout');
+const student_profile = require('./student/profile');
 const bp = require('body-parser');
 const hash = require('password-hash');
 const { check, validationResult } = require('express-validator');
 
 const app = express();
 app.use(student_login);
+app.use(student_logout);
 app.use(student_register);
+app.use(student_profile);
 app.use(bp.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 app.use(session);
 
-
 app.set('view engine','ejs');
 app.use(bp.json());
 
-// APPLY COOKIE SESSION MIDDLEWARE
-/*app.use(session({
-    secret: 'anonymous',
-    resave: true,
-    saveUninitialized: true,
-    user: null
- 
-})); */
-
-
-app.get('/',(req, res)=>{
-    if(req.session.user){
-       req.session.destroy();
-    }
-    res.render('home')
-})
-
-
-
-// LOGOUT
-/*app.get('/logout',(req,res)=>{
-    //session destroy
-    req.session = null;
-    res.redirect('/');
-});*/
-// END OF LOGOUT
-
-
-app.listen(3000, () => console.log("Server is Running..."));
+app.listen(3008, () => console.log("Server is Running..."));
