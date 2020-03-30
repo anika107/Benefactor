5ef2e2796d372b5458603a1c55d4c231d7ee5f59
Anika <32257753+anika107@users.noreply.github.com>
2020-03-30 22:00:26 +0600

    create circular_employer

--- query.sql
+++ query.sql
@@ -16,3 +16,14 @@ ALTER TABLE `student` ADD  FOREIGN KEY (`region1`) REFERENCES `region`(`region_i
 ALTER TABLE `student` ADD  FOREIGN KEY (`region2`) REFERENCES `region`(`region_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE `student` ADD  FOREIGN KEY (`region3`) REFERENCES `region`(`region_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE `student` ADD  FOREIGN KEY (`region4`) REFERENCES `region`(`region_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+alter table circular drop foreign key circular_ibfk_1;alter table circular drop employer_username;
+
+CREATE TABLE `circular_employer` (
+ `circular_id` int(11) DEFAULT NULL,
+ `employer_username` varchar(50) DEFAULT NULL,
+ KEY `circular_id` (`circular_id`),
+ KEY `employer_username` (`employer_username`),
+ CONSTRAINT `circular_employer_ibfk_1` FOREIGN KEY (`circular_id`) REFERENCES `circular` (`circular_id`),
+ CONSTRAINT `circular_employer_ibfk_2` FOREIGN KEY (`employer_username`) REFERENCES `employer` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
